5b1fe8fc94a6a31325b194460de2a85057375434
BeautyCita System <system@beautycita.com>
2025-10-29 07:20:42 +0000

    feat: Phase 2 - Add React Navigation + dependencies with proper configuration

--- mobile-native/android/app/src/main/java/com/beautycitaminimal/MainActivity.kt
+++ mobile-native/android/app/src/main/java/com/beautycitaminimal/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.beautycitaminimal
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  /**
+   * Called when the activity is starting. This is where we handle gesture-handler setup.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
